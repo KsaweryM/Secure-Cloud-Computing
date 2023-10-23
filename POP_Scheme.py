dabfe368a491f2d114b67caad6989f985f2b7874
KsaweryM <ksawery.mozdzynski.kontakt@gmail.com>
2023-10-23 10:51:13 +0200

    POP

--- POP_Scheme.py
+++ POP_Scheme.py
@@ -1,19 +1,19 @@
 import random
 import mcl
 
-from mcl import GT
-from mcl import G2
 from mcl import G1
-from mcl import Fr
 
-# client
-
-z = 2
-secret_key = mcl.Fr()
-secret_key.setByCSPRNG()
-
-G = G1.hashAndMapTo(b"genQ")
-PUBLIC_KEY = G * secret_key
+def read_file(path, chunk_size):
+    divided_file = []
+    nr_chunks = 0
+    with open(path, 'rb') as file:
+        while True:
+            byte = file.read(chunk_size)
+            if not byte:
+                break
+            nr_chunks += 1
+            divided_file.append(int.from_bytes(byte, "big"))
+    return divided_file, hash(path.encode("utf-8")), nr_chunks
 
 def PRNG(secret_key, file_ID, index):
     random.seed(hash(secret_key.getStr()) + hash(file_ID) + hash(index))
@@ -21,45 +21,33 @@ def PRNG(secret_key, file_ID, index):
     random_value.setInt(random.randint(0, 2**32))
     return random_value
 
-def create_polynomial(secret_key, file_ID):
+def create_polynomial(secret_key, file_ID, nr_chunks):
     polynomial = []
-    for i in range(0, z + 1):
+    for i in range(0, nr_chunks + 1):
         polynomial.append(PRNG(secret_key, file_ID, i))
     return polynomial
 
 def print_polynomial(polynomial):
-    for point in polynomial:
-        print(f"a = {point.getStr()}")
+    for i, coefficient in enumerate(polynomial):
+        print(f"a_{i} = {coefficient.getStr()}")
 
-def print_tagged_file(polynomial):
-    for point in polynomial:
-        print(f"m = {point[0].getStr()}, t = {point[1].getStr()}")
-
-def fr_power(argument, exponent):
-    value = mcl.Fr()
-    value.setInt(1)
-    for i in range(exponent):
-        value = value * argument
-    return value
+def print_tagged_file(tagged_file):
+    for i, (m, t) in enumerate(tagged_file):
+        print(f"m_{i} = {m.getStr()}, t = {t.getStr()}")
 
 def execute_polynomial(polynomial, argument):
     value = mcl.Fr()
-    for i in range(0, len(polynomial)):
-        value += fr_power(argument, i) * polynomial[i]
+    for coefficient in polynomial:
+        value = value * argument
+        value = value + coefficient
     return value
-
-def random_file():
-    file = []
-    for i in range(z + 1):
-        file.append(random.randint(0, 2**32))
-    return file
 
 def tag_file(file, polynomial):
     tagged_file = []
     for i in range(len(file)):
         fr_file = mcl.Fr()
         fr_file.setInt(file[i])
-        tagged_file.append((fr_file, execute_polynomial(poly, fr_file)))
+        tagged_file.append((fr_file, execute_polynomial(polynomial, fr_file)))
     return tagged_file
 
 def lagrange_interpolation(tagged_file, x):
@@ -76,11 +64,15 @@ def lagrange_interpolation(tagged_file, x):
         interpolation += multi
     return interpolation
 
-file = random_file()
-file_ID = hash("hello.txt")
+chunk_size = 2
+file, file_ID, nr_chunks = read_file("text.txt", chunk_size)
+secret_key = mcl.Fr()
+secret_key.setByCSPRNG()
 
-poly = create_polynomial(secret_key, file_ID)
+G = G1.hashAndMapTo(b"genQ")
+PUBLIC_KEY = G * secret_key
 
+poly = create_polynomial(secret_key, file_ID, nr_chunks)
 tagged_file = tag_file(file, poly)
 
 x_c = mcl.Fr()
@@ -91,12 +83,10 @@ mcl_zero.setInt(0)
 
 H = (PUBLIC_KEY, x_c, PUBLIC_KEY * (execute_polynomial(poly, mcl_zero)))
 
-Pf = PUBLIC_KEY * execute_polynomial(poly, x_c)
-
+PF = PUBLIC_KEY * execute_polynomial(poly, x_c)
 # client end
 
 #server
-
 psi = []
 psi.append((mcl_zero, H[2]))
 
@@ -104,6 +94,10 @@ for (m, t) in tagged_file:
     psi.append((m, PUBLIC_KEY * t))
 
 RESPONSE = lagrange_interpolation(psi, x_c)
+print(PF.getStr())
 print(RESPONSE.getStr())
+#server end
 
-print(Pf == RESPONSE)
+#client
+print(PF == RESPONSE)
+#client end
